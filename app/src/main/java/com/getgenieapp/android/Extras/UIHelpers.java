0289d1067b1e5df721cadb3f1eee15121d1e5b17
Ravi Gadipudi <rgadipudi@getgenieapp.com>
2015-06-05 23:35:15 -0400

    Researching on How to set text size dynamically for all screen sizes

--- app/src/main/java/com/getgenieapp/android/Extras/UIHelpers.java
+++ app/src/main/java/com/getgenieapp/android/Extras/UIHelpers.java
@@ -1,11 +1,23 @@
 package com.getgenieapp.android.Extras;
 
+import android.app.Activity;
+import android.content.Context;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.graphics.Rect;
+import android.util.TypedValue;
+import android.view.Display;
 import android.widget.TextView;
 
 public class UIHelpers {
-    public float correctWidth(TextView textView, int desiredWidth) {
+    public Point getXYPixels(Activity activity) {
+        Display display = activity.getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        return size;
+    }
+
+    public void correctWidth(TextView textView, Activity activity) {
         Paint paint = new Paint();
         Rect bounds = new Rect();
 
@@ -15,11 +27,23 @@ public class UIHelpers {
         String text = textView.getText().toString();
         paint.getTextBounds(text, 0, text.length(), bounds);
 
-        while (bounds.width() > desiredWidth) {
+        while (bounds.width() > 1080) {
             textSize--;
             paint.setTextSize(textSize);
             paint.getTextBounds(text, 0, text.length(), bounds);
         }
-        return textSize;
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize);
+    }
+
+    public int determineMaxTextSize(String str, float maxWidth)
+    {
+        int size = 0;
+        Paint paint = new Paint();
+
+        do {
+            paint.setTextSize(++ size);
+        } while(paint.measureText(str) < maxWidth);
+
+        return size;
     }
 }
--- app/src/main/java/com/getgenieapp/android/Fragments/RegisterFragment.java
+++ app/src/main/java/com/getgenieapp/android/Fragments/RegisterFragment.java
@@ -7,15 +7,33 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.getgenieapp.android.Extras.UIHelpers;
 import com.getgenieapp.android.R;
 
+import org.w3c.dom.Text;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+
 public class RegisterFragment extends Fragment {
+
+    UIHelpers uiHelpers;
+    @InjectView(R.id.topText)
+    TextView topText;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_register, container, false);
+        ButterKnife.inject(this, rootView);
+
+        uiHelpers = new UIHelpers();
+//        uiHelpers.correctWidth(topText, getActivity());
+
+        topText.setTextSize(uiHelpers.determineMaxTextSize(getActivity().getString(R.string.topText), uiHelpers.getXYPixels(getActivity()).x/4));
+        return rootView;
     }
 }
