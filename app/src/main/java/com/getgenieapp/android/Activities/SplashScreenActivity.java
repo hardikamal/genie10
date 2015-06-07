555e34e01037e2725aae2df9ce3dc7fc9d1d4e02
Ravi Gadipudi <rgadipudi@getgenieapp.com>
2015-06-06 23:40:46 -0400

    GCM Registration process Done. Need to test on 4.4 and receiving msg.

--- app/src/main/java/com/getgenieapp/android/Activities/SplashScreenActivity.java
+++ app/src/main/java/com/getgenieapp/android/Activities/SplashScreenActivity.java
@@ -1,19 +1,30 @@
 package com.getgenieapp.android.Activities;
 
 import android.app.AlertDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.SystemClock;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.getgenieapp.android.Extras.DataFields;
 import com.getgenieapp.android.Extras.Utils;
+import com.getgenieapp.android.GCMHelpers.QuickstartPreferences;
+import com.getgenieapp.android.GCMHelpers.RegistrationIntentService;
 import com.getgenieapp.android.GenieActivity;
 import com.getgenieapp.android.R;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -26,8 +37,12 @@ public class SplashScreenActivity extends GenieActivity {
 
     Utils utils;
 
-    public SplashScreenActivity()
-    {
+    private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+    private static final String TAG = "MainActivity";
+
+    private BroadcastReceiver mRegistrationBroadcastReceiver;
+
+    public SplashScreenActivity() {
         utils = new Utils(this);
     }
 
@@ -37,6 +52,14 @@ public class SplashScreenActivity extends GenieActivity {
         setContentView(R.layout.activity_splash_screen);
         // Butter knife injects all the elements in to objects
         ButterKnife.inject(this);
+        mRegistrationBroadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                SharedPreferences sharedPreferences =
+                        PreferenceManager.getDefaultSharedPreferences(context);
+            }
+        };
+
         try {
             // Version number is set here based on app build if it throws exception it will disappear.
             // Better to hide instead of showing some weird text
@@ -79,6 +102,12 @@ public class SplashScreenActivity extends GenieActivity {
     void runToNextPage() {
         new Thread(new Runnable() {
             public void run() {
+                if (checkPlayServices()) {
+                    // Start IntentService to register this application with GCM.
+                    Intent intent = new Intent(SplashScreenActivity.this, RegistrationIntentService.class);
+                    startService(intent);
+                }
+
                 // wait for x secs specified in datafields class
                 SystemClock.sleep(DataFields.SplashScreenGeneralTimeOut);
                 // check point to check if the token is available and exists.
@@ -88,4 +117,37 @@ public class SplashScreenActivity extends GenieActivity {
             }
         }).start();
     }
+
+    protected void onResume() {
+        super.onResume();
+        LocalBroadcastManager.getInstance(this).registerReceiver(mRegistrationBroadcastReceiver,
+                new IntentFilter(QuickstartPreferences.REGISTRATION_COMPLETE));
+    }
+
+    @Override
+    protected void onPause() {
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mRegistrationBroadcastReceiver);
+        super.onPause();
+    }
+
+    /**
+     * Check the device to make sure it has the Google Play Services APK. If
+     * it doesn't, display a dialog that allows users to download the APK from
+     * the Google Play Store or enable it in the device's system settings.
+     */
+    private boolean checkPlayServices() {
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, this,
+                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            } else {
+                Log.i(TAG, "This device is not supported.");
+                finish();
+            }
+            return false;
+        }
+        return true;
+    }
+
 }
