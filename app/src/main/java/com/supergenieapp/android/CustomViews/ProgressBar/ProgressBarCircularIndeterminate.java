ec6aa9b3de04eb825a469ee16e9347a7669b0996
Ravi Gadipudi <rgadipudi@getgenieapp.com>
2015-08-11 13:42:59 +0530

    Size and color Fixes

--- app/src/main/java/com/supergenieapp/android/CustomViews/ProgressBar/ProgressBarCircularIndeterminate.java
+++ app/src/main/java/com/supergenieapp/android/CustomViews/ProgressBar/ProgressBarCircularIndeterminate.java
@@ -11,13 +11,14 @@ import android.graphics.RectF;
 import android.util.AttributeSet;
 
 import com.supergenieapp.android.Extras.Utils;
+import com.supergenieapp.android.R;
 
 public class ProgressBarCircularIndeterminate extends CustomView {
 
 
     final static String ANDROIDXML = "http://schemas.android.com/apk/res/android";
 
-    int backgroundColor = Color.parseColor("#1E88E5");
+    int backgroundColor = getResources().getColor(R.color.colorPrimary);
 
 
     public ProgressBarCircularIndeterminate(Context context, AttributeSet attrs) {
@@ -27,23 +28,23 @@ public class ProgressBarCircularIndeterminate extends CustomView {
     }
 
     // Set atributtes of XML to View
-    protected void setAttributes(AttributeSet attrs){
+    protected void setAttributes(AttributeSet attrs) {
 
         setMinimumHeight(Utils.dpToPx(32, getResources()));
         setMinimumWidth(Utils.dpToPx(32, getResources()));
 
         //Set background Color
         // Color by resource
-        int bacgroundColor = attrs.getAttributeResourceValue(ANDROIDXML,"background",-1);
-        if(bacgroundColor != -1){
-            setBackgroundColor(getResources().getColor(bacgroundColor));
-        }else{
+        int bacgroundColor = attrs.getAttributeResourceValue(ANDROIDXML, "background", -1);
+        if (bacgroundColor != -1) {
+            setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+        } else {
             // Color by hexadecimal
             int background = attrs.getAttributeIntValue(ANDROIDXML, "background", -1);
             if (background != -1)
                 setBackgroundColor(background);
             else
-                setBackgroundColor(Color.parseColor("#1E88E5"));
+                setBackgroundColor(getResources().getColor(R.color.colorPrimary));
         }
 
         setMinimumHeight(Utils.dpToPx(3, getResources()));
@@ -53,25 +54,26 @@ public class ProgressBarCircularIndeterminate extends CustomView {
 
     /**
      * Make a dark color to ripple effect
+     *
      * @return
      */
-    protected int makePressColor(){
+    protected int makePressColor() {
         int r = (this.backgroundColor >> 16) & 0xFF;
         int g = (this.backgroundColor >> 8) & 0xFF;
         int b = (this.backgroundColor >> 0) & 0xFF;
 //		r = (r+90 > 245) ? 245 : r+90;
 //		g = (g+90 > 245) ? 245 : g+90;
 //		b = (b+90 > 245) ? 245 : b+90;
-        return Color.argb(128,r, g, b);
+        return Color.argb(128, r, g, b);
     }
 
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        if(firstAnimationOver == false)
+        if (firstAnimationOver == false)
             drawFirstAnimation(canvas);
-        if(cont > 0)
+        if (cont > 0)
             drawSecondAnimation(canvas);
         invalidate();
 
@@ -81,38 +83,40 @@ public class ProgressBarCircularIndeterminate extends CustomView {
     float radius2 = 0;
     int cont = 0;
     boolean firstAnimationOver = false;
+
     /**
      * Draw first animation of view
+     *
      * @param canvas
      */
-    private void drawFirstAnimation(Canvas canvas){
-        if(radius1 < getWidth()/2){
+    private void drawFirstAnimation(Canvas canvas) {
+        if (radius1 < getWidth() / 2) {
             Paint paint = new Paint();
             paint.setAntiAlias(true);
-            paint.setColor(makePressColor());
-            radius1 = (radius1 >= getWidth()/2)? (float)getWidth()/2 : radius1+1;
-            canvas.drawCircle(getWidth()/2, getHeight()/2, radius1, paint);
-        }else{
+            paint.setColor(getResources().getColor(R.color.colorPrimary));
+            radius1 = (radius1 >= getWidth() / 2) ? (float) getWidth() / 2 : radius1 + 1;
+            canvas.drawCircle(getWidth() / 2, getHeight() / 2, radius1, paint);
+        } else {
             Bitmap bitmap = Bitmap.createBitmap(canvas.getWidth(), canvas.getHeight(), Bitmap.Config.ARGB_8888);
             Canvas temp = new Canvas(bitmap);
             Paint paint = new Paint();
             paint.setAntiAlias(true);
-            paint.setColor(makePressColor());
-            temp.drawCircle(getWidth()/2, getHeight()/2, getHeight()/2, paint);
+            paint.setColor(getResources().getColor(R.color.colorPrimary));
+            temp.drawCircle(getWidth() / 2, getHeight() / 2, getHeight() / 2, paint);
             Paint transparentPaint = new Paint();
             transparentPaint.setAntiAlias(true);
             transparentPaint.setColor(getResources().getColor(android.R.color.transparent));
             transparentPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
-            if(cont >= 50){
-                radius2 = (radius2 >= getWidth()/2)? (float)getWidth()/2 : radius2+1;
-            }else{
-                radius2 = (radius2 >= getWidth()/2-Utils.dpToPx(4, getResources()))? (float)getWidth()/2-Utils.dpToPx(4, getResources()) : radius2+1;
+            if (cont >= 50) {
+                radius2 = (radius2 >= getWidth() / 2) ? (float) getWidth() / 2 : radius2 + 1;
+            } else {
+                radius2 = (radius2 >= getWidth() / 2 - Utils.dpToPx(4, getResources())) ? (float) getWidth() / 2 - Utils.dpToPx(4, getResources()) : radius2 + 1;
             }
-            temp.drawCircle(getWidth()/2, getHeight()/2, radius2, transparentPaint);
+            temp.drawCircle(getWidth() / 2, getHeight() / 2, radius2, transparentPaint);
             canvas.drawBitmap(bitmap, 0, 0, new Paint());
-            if(radius2 >= getWidth()/2-Utils.dpToPx(4, getResources()))
+            if (radius2 >= getWidth() / 2 - Utils.dpToPx(4, getResources()))
                 cont++;
-            if(radius2 >= getWidth()/2)
+            if (radius2 >= getWidth() / 2)
                 firstAnimationOver = true;
         }
     }
@@ -121,24 +125,26 @@ public class ProgressBarCircularIndeterminate extends CustomView {
     int arcO = 0;
     float rotateAngle = 0;
     int limite = 0;
+
     /**
      * Draw second animation of view
+     *
      * @param canvas
      */
-    private void drawSecondAnimation(Canvas canvas){
-        if(arcO == limite)
-            arcD+=6;
-        if(arcD >= 290 || arcO > limite){
-            arcO+=6;
-            arcD-=6;
+    private void drawSecondAnimation(Canvas canvas) {
+        if (arcO == limite)
+            arcD += 6;
+        if (arcD >= 290 || arcO > limite) {
+            arcO += 6;
+            arcD -= 6;
         }
-        if(arcO > limite + 290){
+        if (arcO > limite + 290) {
             limite = arcO;
             arcO = limite;
             arcD = 1;
         }
         rotateAngle += 4;
-        canvas.rotate(rotateAngle,getWidth()/2, getHeight()/2);
+        canvas.rotate(rotateAngle, getWidth() / 2, getHeight() / 2);
 
         Bitmap bitmap = Bitmap.createBitmap(canvas.getWidth(), canvas.getHeight(), Bitmap.Config.ARGB_8888);
         Canvas temp = new Canvas(bitmap);
@@ -151,19 +157,15 @@ public class ProgressBarCircularIndeterminate extends CustomView {
         transparentPaint.setAntiAlias(true);
         transparentPaint.setColor(getResources().getColor(android.R.color.transparent));
         transparentPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
-        temp.drawCircle(getWidth()/2, getHeight()/2, (getWidth()/2)- Utils.dpToPx(4, getResources()), transparentPaint);
+        temp.drawCircle(getWidth() / 2, getHeight() / 2, (getWidth() / 2) - Utils.dpToPx(4, getResources()), transparentPaint);
 
         canvas.drawBitmap(bitmap, 0, 0, new Paint());
     }
 
-
-
-
-
     // Set color of background
-    public void setBackgroundColor(int color){
+    public void setBackgroundColor(int color) {
         super.setBackgroundColor(getResources().getColor(android.R.color.transparent));
-        if(isEnabled())
+        if (isEnabled())
             beforeBackground = backgroundColor;
         this.backgroundColor = color;
     }
